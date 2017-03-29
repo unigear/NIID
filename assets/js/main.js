3d0031137145bd13b3dd6af5d8ed3c86942db6e4
unigear <arvin@unigear.net>
2017-03-29 15:57:39 +0800

    try

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
+$(document).ready(function() {
 
 (function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
 
 	$(function() {
 
@@ -56,6 +65,6 @@
 
 	});
 
-$(document).ready(function() {
-	
+})(jQuery);
+
 });
